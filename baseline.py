20a3cf0f66075e325d7695a8783f941b99740729
Yuchuan <yyc478@gmail.com>
2018-09-27 16:37:39 +0800

    update baseline

--- baseline.py
+++ baseline.py
@@ -3,45 +3,99 @@ import lmdb
 import os
 import pickle
 import h5py
+import math
+
+# Use GPU 1 since other people are using 0
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+# The GPU id to use, usually either "0" or "1"
+os.environ["CUDA_VISIBLE_DEVICES"]="1" 
 
 np.random.seed(123)
 
-from keras.models import Sequential, Model
-from keras.layers import Dense
+# limit memory usage to play nice with others
+import tensorflow as tf
+from keras.backend.tensorflow_backend import set_session
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+set_session(tf.Session(config=config))
+
+from keras.models import Sequential, Model, load_model
+from keras.layers import Dense, Flatten, Dropout
 from keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
 from keras.preprocessing import image
 from keras.utils.np_utils import to_categorical
 
-resnet = ResNet50(input_shape=(3,256,256), pooling='max', classes=1024, include_top=False, weights=None)
-top_model = Sequential()
-top_model.add(Dense(1024, activation='softmax'))
-model = Model(inputs=resnet.input, outputs=top_model(resnet.output))
+BATCH_SIZE = 20
 
-data_path = 'data/'
-keys_path = 'data/test_keys.pkl'
-images_path = 'data/images/test'
-classes_path = 'data/classes1M.pkl'
-db_path = 'data/data.h5'
+def new_model():
+  resnet = ResNet50(input_shape=(3,256,256), pooling='avg', include_top=False, weights=None)
+  top_model = Sequential()
+  top_model.add(Dense(1048, activation='linear'))
+  top_model.add(Dropout(0.05))
+  top_model.add(Dense(1048, activation='softmax'))
+  model = Model(inputs=resnet.input, outputs=top_model(resnet.output))
+  return model
 
-classes_file = open(classes_path, 'rb')
-id2class = pickle.load(classes_file)
-ind2class = pickle.load(classes_file)
+def open_model(filename):
+  return load_model(filename)
 
-db = h5py.File(db_path, 'r')
+def train():
+  model = open_model('baseline_model_sparse_crossentropy.h5')
+#  model = new_model()
+  data_path = 'data/'
+  keys_path = 'data/test_keys.pkl'
+  images_path = 'data/images/test'
+  classes_path = 'data/classes1M.pkl'
+  db_path = 'data/data.h5'
 
-images = []
-categories = []
-for i in range(10): #actually len(f['ids_train'])
-  id = db['ids_test'][i]
-  category = db['classes_train'][i]
-  for j in range(db['numims_train'][i]):
-    index = db['impos_train'][i][j]
-    image = db['ims_train'][index]
-    images.append(image)
-    categories.append(category)
+  classes_file = open(classes_path, 'rb')
+  id2class = pickle.load(classes_file)
+  ind2class = pickle.load(classes_file)
 
-train_x = np.array(images)
-train_y = to_categorical(categories, 1024)
+  db = h5py.File(db_path, 'r')
 
-model.compile(loss='categorical_crossentropy', optimizer='SGD', metrics=['accuracy'])
-model.fit(train_x, train_y, verbose=1)
+  model.compile(loss='categorical_crossentropy', optimizer='SGD', metrics=['accuracy'])
+  model.fit_generator(
+    batch_generator(db, batch_size=BATCH_SIZE,partition='train'), 
+    validation_data=batch_generator(db, batch_size=BATCH_SIZE, partition='val'),
+    steps_per_epoch=math.floor(238459 / BATCH_SIZE),
+    validation_steps=math.floor(51129 / BATCH_SIZE),
+    epochs=9, 
+    verbose=1
+  )
+
+  model.save('baseline_model_sparse_crossentropy.h5')
+
+def batch_generator(db, batch_size=100, partition='train'):
+  ids = db['ids_{}'.format(partition)]
+  classes = db['classes_{}'.format(partition)]
+  impos = db['impos_{}'.format(partition)]
+  ims = db['ims_{}'.format(partition)]
+  partition_size = len(ids)
+  while(True):
+    images = []
+    categories = []
+    for i in np.random.choice(partition_size, size=batch_size): 
+      id = ids[i]
+      # Since category=0 is the background class, we can ignore that
+      # Experiment with ignoring category 1 (peanut butter, comprising half of
+      # all)
+      category = classes[i]
+      if category == 0 or category == 1:
+        continue
+      category = category - 1
+      for j in range(db['numims_train'][i]):
+        index = db['impos_train'][i][j]
+        image = db['ims_train'][index]
+        images.append(image)
+        categories.append(category)
+
+    batch_x = np.array(images)
+    batch_y = to_categorical(categories, 1048)
+    yield( batch_x, batch_y )
+
+
+def test():
+  model = load_model('baseline_model.h5')
+  
+train()
