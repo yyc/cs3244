6256737e3abbca03503ae1b020d95968cab83af4
derek <namjjd@gmail.com>
2018-10-14 13:20:10 +0800

    Use dataframe to display data for sanity check

--- transform_data_set.py
+++ transform_data_set.py
@@ -8,21 +8,21 @@ FOOD_INGREDIENTS_KEY = 'ingredients'
 FOOD_ID_KEY = 'id'
 INGREDIENT_TEXT_KEY = 'text'
 
-count = 1
+FOOD_PARSING_LIMIT = 10
 
-food_composition = pd.DataFrame
+count = 1
 
 pd_data_values  = []
 pd_data_columns = ['food', 'ingredient']
 
 with open(datafilename, 'r') as f:
-    food = ijs.items(f,'item')
+    food = ijs.items(f, 'item')
 
     for x in food:
         row = json.loads(json.dumps(x))
         food_id = row[FOOD_ID_KEY]
-        ingredients_list = [[food_id,i[INGREDIENT_TEXT_KEY]] for i in row[FOOD_INGREDIENTS_KEY]]
-        
+        ingredients_list = [[food_id, i[INGREDIENT_TEXT_KEY]] for i in row[FOOD_INGREDIENTS_KEY]]
+
         print count
         print food_id
         print ingredients_list
@@ -31,6 +31,8 @@ with open(datafilename, 'r') as f:
 
         count = count + 1
 
-        if count > 10:
+        if count > FOOD_PARSING_LIMIT:
             break
 
+food_compositions = pd.DataFrame(pd_data_values, columns=pd_data_columns)
+print food_compositions.groupby('ingredient')[u'food'].nunique()
