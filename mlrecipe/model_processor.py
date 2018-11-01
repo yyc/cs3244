0e1b6b6444af0ed5366e4956922d59fcea7e6695
derek <namjjd@gmail.com>
2018-11-02 07:15:12 +0800

    Add get_embedding method to food query class

--- mlrecipe/model_processor.py
+++ mlrecipe/model_processor.py
@@ -1,7 +1,8 @@
 """
-Read output of model and output to a json its weights
+Read output of model and perfomr similarity calculation
 """
 
+import argparse
 import ijson as ijs
 import json
 from keras.layers import Input, Embedding, Dot, Reshape, Dense
@@ -9,25 +10,36 @@ from keras.models import Model, load_model
 import numpy as np
 import pandas as pd
 
-JSON_DUMP_PATH = "json/embeddings.json"
-
-MODEL_PATH = "models/model.h5"
 FOOD_EMBEDDING_LAYER_NAME = "food_embedding"
 
-food_ingredient_model = load_model(MODEL_PATH)
-food_ingredient_model.summary()
+def extract_food_weights_from_model(food_ingredient_model):
+    food_layer = food_ingredient_model.get_layer(FOOD_EMBEDDING_LAYER_NAME)
+    food_weights = food_layer.get_weights()[0]
 
-food_layer = food_ingredient_model.get_layer(FOOD_EMBEDDING_LAYER_NAME)
-food_weights = food_layer.get_weights()[0]
-food_weights.shape
+    weights = food_weights / np.linalg.norm(food_weights, axis = 1).reshape((-1, 1))
 
-food_weights = food_weights / np.linalg.norm(food_weights, axis = 1).reshape((-1, 1))
+    return weights
 
-with open(JSON_DUMP_PATH, 'w') as outfile:
-    json.dump(pd.DataFrame(food_weights).to_dict(), outfile)
+#with open(JSON_DUMP_PATH, 'w') as outfile:
+#    json.dump(pd.DataFrame(food_weights).to_dict(), outfile)
 
 #INDEX_TO_FIND = 0
 #dists = np.dot(food_weights, food_weights[INDEX_TO_FIND])
 #sorted_dists = np.argsort(-dists)
 
-import pdb; pdb.set_trace()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Model Processor')
+    parser.add_argument('-m', '--model_path', dest='model_path')
+    args = parser.parse_args()
+
+    food_ingredient_model = load_model(args["model_path"])
+    food_ingredient_model.summary()
+
+    food_weights = extract_food_weights_from_model(food_ingredient_model)
+
+    # Given 2 arrays of ids to compare, return their similarity index
+
+    list1 =
+    list2 =
+
+    z
