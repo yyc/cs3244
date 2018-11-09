fcde8811cfac62d4dbea22cf982926717767f116
Yuchuan <yyc478@gmail.com>
2018-11-09 18:49:54 +0800

    Add analysis to data_extract to find good category subset

--- data_extract.py
+++ data_extract.py
@@ -3,7 +3,7 @@ import os
 import pickle
 import h5py
 import math
-
+import collections
 
 def batch_extractor(db, new_db, batch_size=100, partition='train'):
   ids = db['ids_{}'.format(partition)]
@@ -29,12 +29,31 @@ def batch_extractor(db, new_db, batch_size=100, partition='train'):
   new_db.create_dataset('ims_{}'.format(partition), data=[ims[id - 1] for id in new_img_ids])
   return new_db
 
+def freq_analyze(db, categories, fsq, partition="train"):
+  classes = db['classes_{}'.format(partition)]
+  ids = db['ids_{}'.format(partition)]
+  numrecipes = len(ids)
+  filtered_classes = [classes[i] for i in range(numrecipes) if ids[i].decode("utf-8") in fsq]
+  print("Filtered recipes {} of {}".format(len(filtered_classes), numrecipes))
+  counter = collections.Counter(filtered_classes)
+  most_common_50 = counter.most_common(50)
+  for c in most_common_50:
+    print("{} \t {} \t {}".format(c[0], categories[c[0] - 1], c[1]))
   
 db_path = 'data/data.h5'
-new_db_path = 'data/sample_val_data.h5'
+new_db_path = 'data/sample_train_data.h5'
+classes_file = "data/classes1M.pkl"
 
 db = h5py.File(db_path, 'r')
-new_db = h5py.File(new_db_path, 'w')
+# new_db = h5py.File(new_db_path, 'w')
 
-batch_extractor(db, new_db, batch_size=1000, partition="val")
-new_db.flush()
+fsq_id_query = pickle.load(open('mlrecipe/food_id_to_int.p', "rb"))
+
+# batch_extractor(db, new_db, batch_size=1000, partition="val")
+# new_db.flush()
+
+classes = open(classes_file, "rb")
+recipes = pickle.load(classes)
+classes_dict = pickle.load(classes)
+
+freq_analyze(db, classes_dict, fsq_id_query, partition="train")
