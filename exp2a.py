8441eb7c41423a6c6029dc6d81c3900c04951ab7
Yuchuan <yyc478@gmail.com>
2018-11-11 22:41:20 +0800

    Add 2a with augmentation

--- exp2a.py
+++ exp2a.py
@@ -9,13 +9,15 @@ import math
 
 from keras.callbacks import ModelCheckpoint
 from keras.optimizers import SGD
+from keras_preprocessing.image import ImageDataGenerator
+from keras import backend as K
 
 from mlrecipe.food_similarity_query import FoodSimilarityQuery
 
 # Use GPU 1 since other people are using 0
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
 # The GPU id to use, usually either "0" or "1"
-os.environ["CUDA_VISIBLE_DEVICES"]="1" 
+os.environ["CUDA_VISIBLE_DEVICES"]="1"
 
 np.random.seed(123)
 
@@ -32,7 +34,7 @@ keras.backend.set_image_data_format("channels_first")
 from keras.models import Sequential, Model, load_model
 from keras.layers import Dense, Flatten, Dropout
 from keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
-from keras.preprocessing import image
+from keras.preprocessing.image import ImageDataGenerator
 from keras.utils.np_utils import to_categorical
 
 BATCH_SIZE = 20
@@ -40,7 +42,14 @@ DEFAULT_MODEL_PATH = 'exp2a_model.h5'
 DEFAULT_NUM_EPOCH = 40
 NUM_INGRE = 50 # Embedding of size 50
 
-def new_model():
+# Helper for cosine similarity
+def cos_distance(y_true, y_pred):
+  y_true = K.l2_normalize(y_true, axis=-1)
+  y_pred = K.l2_normalize(y_pred, axis=-1)
+  return K.mean(1 - K.sum((y_true * y_pred), axis=-1))
+
+
+def new_model(top_layers, dropout):
   # See https://keras.io/applications/#fine-tune-inceptionv3-on-a-new-set-of-classes
   resnet = ResNet50(input_shape=(3,256,256), pooling='avg', include_top=False, weights='imagenet')
   # Freeze resnet layers
@@ -56,32 +65,38 @@ def new_model():
   ingre_category = Dense(NUM_INGRE, activation='softmax', name='ingre_category')(x)
 
   y = keras.layers.concatenate([resnet.output, ingre_category])
-  y = Dense(2048, activation='relu')(y)
-  y = Dropout(0.2)(y)
-  y = Dense(1048, activation='relu')(y)
-  y = Dropout(0.2)(y)
-  food_category = Dense(1048, activation='softmax', name='food_category')(y)
+  for l in top_layers:
+    y = Dense(l, activation='relu')(y)
+    y = Dropout(dropout)(y)
+  food_category = Dense(16, activation='softmax', name='food_category')(y)
 
   return Model(inputs=resnet.input, outputs=[ingre_category, food_category])
 
 def open_model(filename):
   return load_model(filename)
 
-def train(model_path=None, num_epoch=DEFAULT_NUM_EPOCH, checkpoint_path=None, learning_rate = 0.01, momentum=0.9):
+def train(model_path=None, num_epoch=DEFAULT_NUM_EPOCH, checkpoint_path=None, learning_rate = 0.01, momentum=0.9, augment=False, weights_path=None):
   # Set directory for model load and save:
   if model_path is None:
     model_path = DEFAULT_MODEL_PATH
-  if os.path.exists(model_path):
-    model = open_model(model_path)
-  else:
+
+  if weights_path and os.path.exists(weights_path):
     model = new_model()
+    model.load_weights(weights_path)
+    print("Initialized new model and loaded weights from {}".format(weights_path))
+  elif os.path.exists(model_path):
+    model = open_model(model_path)
+    print("Opened existing model {}".format(model_path))
+  else:
+    model = new_model([1048, 1048, 512, 512], 0.2)
+    print("initialized new model")
 
   # Set default directories
   data_path = 'data/'
   keys_path = 'data/test_keys.pkl'
   images_path = 'data/images/test'
   classes_path = 'data/classes1M.pkl'
-  db_path = 'data/data.h5'
+  db_path = 'data/subset_train_val.h5'
 
   # classes_file = open(classes_path, 'rb')
   # id2class = pickle.load(classes_file)
@@ -97,13 +112,13 @@ def train(model_path=None, num_epoch=DEFAULT_NUM_EPOCH, checkpoint_path=None, le
 
   optimizer = SGD(lr=learning_rate, momentum=momentum)
 
-  model.compile(loss={'ingre_category': 'cosine_proximity',
+  model.compile(loss={'ingre_category': cos_distance,
                       'food_category': 'categorical_crossentropy'},
-                loss_weights={'ingre_category': 1,
-                      'food_category': 9},
+                # loss_weights={'ingre_category': 1,
+                #       'food_category': 9},
                 optimizer=optimizer, metrics=['accuracy'])
   model.fit_generator(
-    batch_generator(db, fsq, batch_size=BATCH_SIZE,partition='train'),
+    batch_generator(db, fsq, batch_size=BATCH_SIZE,partition='train', augment=augment),
     validation_data=batch_generator(db, fsq, batch_size=BATCH_SIZE, partition='val'),
     steps_per_epoch=math.floor(238459 / BATCH_SIZE),
     validation_steps=math.floor(51129 / BATCH_SIZE),
@@ -114,7 +129,7 @@ def train(model_path=None, num_epoch=DEFAULT_NUM_EPOCH, checkpoint_path=None, le
 
   model.save(model_path)
 
-def batch_generator(db, fsq, batch_size=100, partition='train'):
+def batch_generator(db, fsq, batch_size=100, partition='train', augment=False):
   # TODO: add ingredient result in output, format: outputs=[ingre_category, food_category]
   ids = db['ids_{}'.format(partition)]
   classes = db['classes_{}'.format(partition)]
@@ -123,6 +138,18 @@ def batch_generator(db, fsq, batch_size=100, partition='train'):
   numims = db['numims_{}'.format(partition)]
   # ingrs = db['ingrs_{}'.format(partition)]
   partition_size = len(ids)
+  if augment:
+      augmenter = ImageDataGenerator(
+          data_format="channels_first",
+          horizontal_flip=True,
+          zoom_range=0.1,
+          width_shift_range=0.2,
+          height_shift_range=0.2,
+          fill_mode="constant"
+      )
+      im_shape = ims[0].shape
+      random_augmenters = [augmenter.get_random_transform(im_shape) for i in range(1000)]
+      print("Created augmenter")
   while(True):
     images = []
     categories = []
@@ -144,12 +171,17 @@ def batch_generator(db, fsq, batch_size=100, partition='train'):
         continue
       for j in range(numims[i]):
         index = impos[i][j] - 1
-        image = ims[index]
+        # Only augment 1/5 of the time
+        if augment and np.random.randint(5) >= 4:
+          opts = np.random.choice(random_augmenters, 1)[0]
+          image = augmenter.apply_transform(ims[index], opts)
+        else:
+          image = ims[index]
         images.append(image)
         ingredients.append(ingr)
         categories.append(category)
     batch_x = np.array(images)
-    batch_y = to_categorical(categories, 1048)
+    batch_y = to_categorical(categories, 16)
 
     yield (batch_x, {'ingre_category': np.array(ingredients), 'food_category': batch_y})
 
@@ -162,5 +194,9 @@ if __name__ == '__main__':
   parser.add_argument('-p', '--model_path', dest='model_path')
   parser.add_argument('-e', '--num_epoch', dest='num_epoch', default=DEFAULT_NUM_EPOCH, type=int)
   parser.add_argument('-c', '--checkpoint', dest='checkpoint_path')
+  parser.add_argument('-w', '--weights', dest="weights_path")
+  parser.add_argument('-a', '--augment', dest='augment', default=False, type=bool)
+  parser.add_argument('-lr', dest='learning_rate', type=float, default=0.02)
+  parser.add_argument('-m', dest='momentum', type=float, default=0.9)
   args = parser.parse_args()
   train(**vars(args))
